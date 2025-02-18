0da909a5d14fdc00327757477f540b30db38b912
mike kausch <kauschie223@yahoo.com>
2025-02-18 15:49:56 -0800

    updated reading data, added normalization function

--- lab3/flores_kausch_lab3.py
+++ lab3/flores_kausch_lab3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import csv
 import matplotlib
 matplotlib.use('TkAgg')
@@ -26,11 +27,10 @@ def read_steamPurchase(filepath):
             # print()
         for i in range(len(headers)):
             data[headers[i]]=persons[i]
-        print(data)
-        print()
+        # print(data)
+        # print()
         
     return data
-
 def read_steamList(filepath):
     data = {}
     with open(filepath, 'r', newline='', encoding='utf-8-sig') as file:
@@ -43,31 +43,71 @@ def read_steamList(filepath):
             genre.append(row[2])
         data = {headers[0]:app_id, headers[1]:name, headers[2]:genre}
 
-        print(data)
-        print()
+        # print(data)
+        # print()
     return data
-
 # Dataset order: Sepal length, Sepal width, Petal length, Petal width, Species 
 def read_iris(filepath):
     data = {}
     with open(filepath, 'r', newline='', encoding='utf-8-sig') as file:
         reader = csv.reader(file)
         s_len, s_wid, p_len, p_wid, species = [], [], [], [], []
+        g1, g2, g3 = {}, {}, {}
+
+        prev_class = None
+        g2_start = None
+        g3_start = None
+
+        count = 0
         headers = ["Sepal length", "Sepal width", "Petal length", "Petal width", "Species"]  # Read header row
         for row in reader:
-            s_len.append(float(row[0]))
-            s_wid.append(float(row[1]))
-            p_len.append(float(row[2]))
-            p_wid.append(float(row[3]))
-            species.append(float(row[4]))
-        data = {headers[0]:s_len, 
+            f1 = float(row[0])
+            f2 = float(row[1])
+            f3 = float(row[2])
+            f4 = float(row[3])
+            s = row[4]
+
+            # init prev_class
+
+            if prev_class == None:
+                prev_class = s
+            elif prev_class != s:
+                if g2_start == None:
+                    g2_start = count
+                    prev_class = s
+                elif g3_start == None:
+                    g3_start = count
+                    prev_class = s
+
+            s_len.append(f1)
+            s_wid.append(f2)
+            p_len.append(f3)
+            p_wid.append(f4)
+            species.append(s)
+            count += 1
+                
+        all = {headers[0]:s_len, 
                 headers[1]:s_wid, 
                 headers[2]:p_len,
                 headers[3]:p_wid,
                 headers[4]:species}
+        g1 = {headers[0]:s_len[:g2_start], 
+                headers[1]:s_wid[:g2_start], 
+                headers[2]:p_len[:g2_start],
+                headers[3]:p_wid[:g2_start]}
+        g2 = {headers[0]:s_len[g2_start:g3_start], 
+                headers[1]:s_wid[g2_start:g3_start], 
+                headers[2]:p_len[g2_start:g3_start],
+                headers[3]:p_wid[g2_start:g3_start]}
+        g3 = {headers[0]:s_len[g3_start:], 
+                headers[1]:s_wid[g3_start:], 
+                headers[2]:p_len[g3_start:],
+                headers[3]:p_wid[g3_start:],}
+        
+        
 
-        print(data)
-        print()
+        data = {"all": all, "g1": g1, "g2":g2, "g3":g3}
+        # print(data)
     return data
 
 def read_judges(filepath):
@@ -99,9 +139,29 @@ def read_judges(filepath):
         j_data = {"Judge1":judges[0], "Judge2":judges[1], "Judge3":judges[2], "Judge4":judges[3], "Judge5":judges[4], "Judge6":judges[5]}
         data = {"Persons":p_data, "Judges":j_data}
 
-        print(data)
-        print()
+        # print(data)
+        # print()
     return data
+
+def min_max_norm(list_of_vals):
+    l_max = max(list_of_vals)
+    l_min = min(list_of_vals)
+    normalized_vals = []
+    for v in list_of_vals:
+        normalized_vals.append((v-l_min)/(l_max-l_min))
+
+    print(f"min: {l_min}")
+    print(f"max: {l_max}")
+    return normalized_vals
+
+
+def preprocess_iris():
+    # outlier detection (intra-group)
+
+    # min/max normalization (all group)
+
+    # return preprocessed data
+    pass
 
 def main():
     # need nxn matrix for all
@@ -109,6 +169,12 @@ def main():
     steamListData = read_steamList(steam_list_path)
     irisData = read_iris(iris_path)
     judgeData = read_judges(judge_path)
+
+    data = irisData['all']['Sepal length']
+    norms = min_max_norm(data)
+    print(f"data: {data}")
+    print(f"norms: {norms}")
+
 
 if __name__ == "__main__":
     main()
